2a2663709f2f3d9ebb5383975b0be8ad8488d217
joelk <joelkinsell@gmail.com>
2019-02-15 10:52:02 +0100

    Initial commit

---
+++ M2P/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class M2PConfig(AppConfig):
+    name = 'M2P'
---
+++ Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\joelk\Desktop\GEI\GEI-3\2nPar\Models de Proces\Meet2Play\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
