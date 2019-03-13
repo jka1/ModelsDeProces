7a53f1de7e2fd49bc967600ac8aa6726eb4e635b
viloc98 <viloc98@gmail.com>
2019-03-13 10:28:18 +0100

    creada carpeta templates

--- M2P/urls.py
+++ M2P/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import url
 from . import views
 
-
+app_name = 'M2P'
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'esports/', views.esports, name='esports'),
 ]
--- M2P/views.py
+++ M2P/views.py
@@ -5,4 +5,17 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    return render(request, 'index.html')
+
+
+def esports(request):
+    return HttpResponse("Aixo serà la pàgina dels esports.")
+
+
+def pista(request):
+    response = "Aixo serà la pàgina de una pista"
+    return HttpResponse(response)
+
+
+def event(request):
+    return HttpResponse("Això sera la pagina de un event.")
