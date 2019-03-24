6fe355dc38f31b9308a7f107a5d1463e6fd939c8
viloc98 <viloc98@gmail.com>
2019-03-24 21:38:05 +0100

    Millorat home page

--- M2P/admin.py
+++ M2P/admin.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
-from  .models import Sport, Event, Location
-
+from .models import Sport, Event, Location, DateTime
 
 admin.site.register(Sport)
 admin.site.register(Event)
 admin.site.register(Location)
+admin.site.register(DateTime)
